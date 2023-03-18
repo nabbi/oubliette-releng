8a87845374d599c67e667738a3b38be7fabcfcf6
Nic Boet <nic@boet.cc>
2023-03-18 10:26:26 -0500

    improve stage3 fetch and build
    Signed-off-by: Nic Boet <nic@boet.cc>

--- tools/download-stage3-seed.sh
+++ tools/download-stage3-seed.sh
@@ -1,15 +1,18 @@
 #! /usr/bin/env bash
+# naming upstreams inital seed stage3 as our own speeds up
+# session resume without need to tweak the spec files
 
 URL='https://distfiles.gentoo.org/releases/amd64/autobuilds/current-stage3-amd64-hardened-openrc/'
 
 SEED=$(curl -s $URL | grep "stage3-amd64-hardened-openrc.*.tar.xz<" | sed 's/^.*href="//' | sed 's/">.*$//')
+MYSEED=$(echo $SEED | sed 's/amd64-hardened/amd64-oubliette-hardened/')
 
 DIR=/var/tmp/catalyst/builds/hardened
 mkdir -p $DIR > /dev/null
 
-if [[ ! -f $DIR/$SEED ]]; then
+if [[ ! -f $DIR/$MYSEED ]]; then
     echo "Fetching new $SEED"
-    curl -s $URL/$SEED -o $DIR/$SEED
+    curl -s $URL/$SEED -o $DIR/$MYSEED
 else
-    echo "$SEED is already latest"
+    echo "$SEED is alreay Gentoo's latest autobuild"
 fi
